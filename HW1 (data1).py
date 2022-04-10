9e2d0c4875cfdf9188a8fcf17319139478a49ac6
HYJ507 <102612818+HYJ507@users.noreply.github.com>
2022-04-10 17:09:33 -0500

    Create HW1 (data1).py

---
+++ HW1 (data1).py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  9 16:49:37 2022
+
+@author: jackyhuang
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  5 10:05:41 2022
+
+@author: jackyhuang
+"""
+
+# PPHA 30535
+# Spring 2021
+# Homework 1
+
+# Yijie Huang
+# Yijie Huang
+
+# Due date: Sunday April 10th before midnight
+# Write your answers in the space between the questions, and commit/push only this file to your repo.
+# Note that there can be a difference between giving a "minimally" right answer, and a really good
+# answer, so it can pay to put thought into your work.
+
+#############
+
+# Question 1: Using a for loop, write code that takes in any list of objects, then prints out:
+# "The value at position __ is __" for every element in the loop, where the first blank is the
+# index location and the second blank the object at that index location.
+
+my_dict = ['Boston', 'Syracuse', 'NewYork','Chicago']
+
+for l in my_dict:
+    print('The value at position' + str(my_dict.index(l)) + 'is'+ str(l))
+
+#https://www.geeksforgeeks.org/python-list-index/
+
+# Question 2: A palindrome is a word or phrase that is the same both forwards and backwards. Write
+# code that takes a variable of any string, then tests to see whether it qualifies as a palindrome.
+# Make sure it counts the word "radar" and the phrase "A man, a plan, a canal, Panama!", while
+# rejecting the word "Apple" and the phrase "This isn't a palindrome". Print the results of these
+# four tests.
+
+
+
+#radar
+def isPalindrome(s):
+    return l == l[::-1]
+
+l = 'radar'
+ans = isPalindrome(l)
+
+if(ans):
+    print('Yes')
+else:
+    print('No')
+
+
+
+#A man, a plan, a canal, Panama!
+
+def is_palindrome(z):
+    z= z.casefold()
+    z= reversed(z)
+    z= 'A man, a plan, a canal, Panama!'
+    
+if list(z) == list(z):
+   print("Yes")
+else:
+   print("No")
+
+  
+    
+#apple
+def isPalindrome(a):
+    return a == a[::-1]
+
+a = 'apple'
+ans = isPalindrome(a)
+
+if(ans):
+    print('Yes')
+else:
+    print('No')
+
+
+#This isn't a palindrome
+def isPalindrome(b):
+    return b == b[::-1]
+
+b = "This isn't a palindrome"
+ans = isPalindrome(b)
+
+if(ans):
+    print('Yes')
+else:
+    print('No')
+
+#https://www.geeksforgeeks.org/sentence-palindrome-palindrome-removing-spaces-dots-etc/
+
+# Question 3: The code below pauses to wait for user input, before assigning the user input to the
+# variable.  Beginning with the given code, check to see if the answer given is an available
+# vegetable.  If it is, print that the user can have the vegetable and end the bit of code.  If
+# they input something unrecognized by our list, tell the user they made an invalid choice and make
+# them pick again.  Repeat until they pick a valid vegetable.
+available_vegetables = ['carrot', 'kale', 'radish', 'pepper']
+choice = input('Please pick a vegetable I have available: ')
+
+
+while True: 
+    choice1 = input('please pick a vegetable')
+   
+    if choice1 in available_vegetables:
+        print("Yes,I got it!")
+        break
+    else:
+        print("Please pick a vegetable I have available",available_vegetables)
+      
+
+
+
+#http://introtopython.org/while_input.html
+
+# Question 4: Write a list comprehension that starts with any list of strings, and returns a new
+# list that contains each string in all lower-case letters, but only if the string begins with the
+# letter "a" or "b".
+
+City = ['Chicago','Seattle','Syracuse','New York', 'Arizon','Boston']
+
+
+
+new_city = [x.lower() for x in City if x.startswith("A") or x.startswith("B")]
+
+print(new_city)
+
+#https://www.programiz.com/python-programming/methods/string/startswith
+
+# Question 5: Beginning with the list below, write a single list comprehension that turns it into
+# the following list: [26, 22, 18, 14, 10, 6]
+start_list = [3, 5, 7, 9, 11, 13]
+
+
+new_list5 = [x*2 for x in reversed(start_list)]
+
+
+print(new_list5)
+
+#https://www.programiz.com/python-programming/methods/list/reverse
+
+# Question 6: Beginning with the two lists below, write a single dictionary comprehension that
+# turns them into the following dictionary: {'IL':'Illinois', 'IN':'Indiana', 'MI':'Michigan', 'OH':'Ohio'}
+short_names = ['IL', 'IN', 'MI', 'OH']
+long_names  = ['Illinois', 'Indiana', 'Michigan', 'Ohio']
+
+dict_state = dict(zip(short_names, long_names))
+
+print(dict_state)
+
+
+#https://www.programiz.com/python-programming/methods/built-in/zip
+
